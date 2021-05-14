f93b695b878d6e3c9f31031faa839c30d162e3aa
quadraticmuffin <55665157+quadraticmuffin@users.noreply.github.com>
2021-05-14 00:00:16 -0400

    separate benchmarking tests by matrix, tensor type

--- cpp/benchmark_matrix.cpp
+++ cpp/benchmark_matrix.cpp
@@ -1,7 +1,47 @@
 #include "matrix_walker.h"
 #include <iostream>
 
-void matrix_rand_move(matrix_walker *matrix) {
+void matrix_rand_move_naive(matrix_walker *matrix) {
+    matrix->teleport((matrix->size() - 1) / 2, (matrix->size() - 1) / 2);
+	for (int i; i < 1e7; i++) {
+		int r = rand() % 4;
+		switch(r){
+			case 0:
+				matrix->move_up();
+				break;
+			case 1:
+				matrix->move_down();
+				break;
+			case 2:
+				matrix->move_left();
+				break;
+			case 3:
+				matrix->move_right();
+				break;
+		}
+	}
+}
+void matrix_rand_move_z(matrix_walker *matrix) {
+    matrix->teleport((matrix->size() - 1) / 2, (matrix->size() - 1) / 2);
+	for (int i; i < 1e7; i++) {
+		int r = rand() % 4;
+		switch(r){
+			case 0:
+				matrix->move_up();
+				break;
+			case 1:
+				matrix->move_down();
+				break;
+			case 2:
+				matrix->move_left();
+				break;
+			case 3:
+				matrix->move_right();
+				break;
+		}
+	}
+}
+void matrix_rand_move_hilbert(matrix_walker *matrix) {
     matrix->teleport((matrix->size() - 1) / 2, (matrix->size() - 1) / 2);
 	for (int i; i < 1e7; i++) {
 		int r = rand() % 4;
@@ -22,7 +62,33 @@ void matrix_rand_move(matrix_walker *matrix) {
 	}
 }
 
-void matrix_row_major(matrix_walker *matrix) {
+void matrix_row_major_naive(matrix_walker *matrix) {
+	matrix->teleport(0, 0);
+	for (int i = 0; i < matrix->size() / 2; i++) {
+		for (int j = 0; j < matrix->size() - 1; j++) {
+			matrix->move_right();
+		}
+		matrix->move_down();
+		for (int j = matrix->size() - 1; j > 0; j--) {
+			matrix->move_left();
+		}
+		matrix->move_down();
+	}
+}
+void matrix_row_major_z(matrix_walker *matrix) {
+	matrix->teleport(0, 0);
+	for (int i = 0; i < matrix->size() / 2; i++) {
+		for (int j = 0; j < matrix->size() - 1; j++) {
+			matrix->move_right();
+		}
+		matrix->move_down();
+		for (int j = matrix->size() - 1; j > 0; j--) {
+			matrix->move_left();
+		}
+		matrix->move_down();
+	}
+}
+void matrix_row_major_hilbert(matrix_walker *matrix) {
 	matrix->teleport(0, 0);
 	for (int i = 0; i < matrix->size() / 2; i++) {
 		for (int j = 0; j < matrix->size() - 1; j++) {
@@ -36,7 +102,7 @@ void matrix_row_major(matrix_walker *matrix) {
 	}
 }
 
-void matrix_col_major(matrix_walker *matrix) {
+void matrix_col_major_naive(matrix_walker *matrix) {
 	matrix->teleport(0, 0);
 	for (int i = 0; i < matrix->size() / 2; i++) {
 		for (int j = 0; j < matrix->size() - 1; j++) {
@@ -49,6 +115,33 @@ void matrix_col_major(matrix_walker *matrix) {
 		matrix->move_right();
 	}
 }
+void matrix_col_major_z(matrix_walker *matrix) {
+	matrix->teleport(0, 0);
+	for (int i = 0; i < matrix->size() / 2; i++) {
+		for (int j = 0; j < matrix->size() - 1; j++) {
+			matrix->move_down();
+		}
+		matrix->move_right();
+		for (int j = matrix->size() - 1; j > 0; j--) {
+			matrix->move_up();
+		}
+		matrix->move_right();
+	}
+}
+void matrix_col_major_hilbert(matrix_walker *matrix) {
+	matrix->teleport(0, 0);
+	for (int i = 0; i < matrix->size() / 2; i++) {
+		for (int j = 0; j < matrix->size() - 1; j++) {
+			matrix->move_down();
+		}
+		matrix->move_right();
+		for (int j = matrix->size() - 1; j > 0; j--) {
+			matrix->move_up();
+		}
+		matrix->move_right();
+	}
+}
+
 
 int main() {
 	#ifdef DEBUG
@@ -71,15 +164,15 @@ int main() {
 	hm->set_default();
 
 	fprintf(stderr, "Benchmarking naive_matrix_walker...\n");
-	matrix_rand_move(nm);
-	matrix_row_major(nm);
-	matrix_col_major(nm);
+	matrix_rand_move_naive(nm);
+	matrix_row_major_naive(nm);
+	matrix_col_major_naive(nm);
 	fprintf(stderr, "Benchmarking z_matrix_walker...\n");
-	matrix_rand_move(zm);
-	matrix_row_major(zm);
-	matrix_col_major(zm);
+	matrix_rand_move_z(zm);
+	matrix_row_major_z(zm);
+	matrix_col_major_z(zm);
 	fprintf(stderr, "Benchmarking hilbert_matrix_walker...\n");
-	matrix_rand_move(hm);
-	matrix_row_major(hm);
-	matrix_col_major(hm);
+	matrix_rand_move_hilbert(hm);
+	matrix_row_major_hilbert(hm);
+	matrix_col_major_hilbert(hm);
 }
--- cpp/benchmark_tensor.cpp
+++ cpp/benchmark_tensor.cpp
@@ -1,7 +1,59 @@
 #include "tensor_walker.h"
 #include <iostream>
 
-void tensor_rand_move(tensor_walker *tensor) {
+void tensor_rand_move_naive(tensor_walker *tensor) {
+    tensor->teleport((tensor->size() - 1) / 2, (tensor->size() - 1) / 2, (tensor->size() - 1) / 2);
+	for (int i; i < 1e7; i++) {
+		int r = rand() % 6;
+		switch(r){
+			case 0:
+				tensor->move_up();
+				break;
+			case 1:
+				tensor->move_down();
+				break;
+			case 2:
+				tensor->move_left();
+				break;
+			case 3:
+				tensor->move_right();
+				break;
+			case 4:
+				tensor->move_in();
+				break;
+			case 5:
+				tensor->move_out();
+				break;
+		}
+	}
+}
+void tensor_rand_move_z(tensor_walker *tensor) {
+    tensor->teleport((tensor->size() - 1) / 2, (tensor->size() - 1) / 2, (tensor->size() - 1) / 2);
+	for (int i; i < 1e7; i++) {
+		int r = rand() % 6;
+		switch(r){
+			case 0:
+				tensor->move_up();
+				break;
+			case 1:
+				tensor->move_down();
+				break;
+			case 2:
+				tensor->move_left();
+				break;
+			case 3:
+				tensor->move_right();
+				break;
+			case 4:
+				tensor->move_in();
+				break;
+			case 5:
+				tensor->move_out();
+				break;
+		}
+	}
+}
+void tensor_rand_move_hilbert(tensor_walker *tensor) {
     tensor->teleport((tensor->size() - 1) / 2, (tensor->size() - 1) / 2, (tensor->size() - 1) / 2);
 	for (int i; i < 1e7; i++) {
 		int r = rand() % 6;
@@ -28,7 +80,61 @@ void tensor_rand_move(tensor_walker *tensor) {
 	}
 }
 
-void tensor_ijk_order(tensor_walker *tensor) {
+void tensor_ijk_order_naive(tensor_walker *tensor) {
+	tensor->teleport(0, 0, 0);
+	for (int i = 0; i < tensor->size() / 2; i++) {
+		for (int j = 0; j < tensor->size() / 2; j++) {
+			for (int k = 0; k < tensor->size() - 1; k++) {
+				tensor->move_right();
+			}
+			tensor->move_down();
+			for (int k = tensor->size() - 1; k > 0; k--) {
+				tensor->move_left();
+			}
+			tensor->move_down();
+		}
+		tensor->move_out();
+		for (int j = 0; j < tensor->size() / 2; j++) {
+			for (int k = 0; k < tensor->size() - 1; k++) {
+				tensor->move_right();
+			}
+			tensor->move_up();
+			for (int k = tensor->size() - 1; k > 0; k--) {
+				tensor->move_left();
+			}
+			tensor->move_up();
+		}
+		tensor->move_out();
+	}
+}
+void tensor_ijk_order_z(tensor_walker *tensor) {
+	tensor->teleport(0, 0, 0);
+	for (int i = 0; i < tensor->size() / 2; i++) {
+		for (int j = 0; j < tensor->size() / 2; j++) {
+			for (int k = 0; k < tensor->size() - 1; k++) {
+				tensor->move_right();
+			}
+			tensor->move_down();
+			for (int k = tensor->size() - 1; k > 0; k--) {
+				tensor->move_left();
+			}
+			tensor->move_down();
+		}
+		tensor->move_out();
+		for (int j = 0; j < tensor->size() / 2; j++) {
+			for (int k = 0; k < tensor->size() - 1; k++) {
+				tensor->move_right();
+			}
+			tensor->move_up();
+			for (int k = tensor->size() - 1; k > 0; k--) {
+				tensor->move_left();
+			}
+			tensor->move_up();
+		}
+		tensor->move_out();
+	}
+}
+void tensor_ijk_order_hilbert(tensor_walker *tensor) {
 	tensor->teleport(0, 0, 0);
 	for (int i = 0; i < tensor->size() / 2; i++) {
 		for (int j = 0; j < tensor->size() / 2; j++) {
@@ -56,7 +162,61 @@ void tensor_ijk_order(tensor_walker *tensor) {
 	}
 }
 
-void tensor_kji_order(tensor_walker *tensor) {
+void tensor_kji_order_naive(tensor_walker *tensor) {
+	tensor->teleport(0, 0, 0);
+	for (int k = 0; k < tensor->size() / 2; k++) {
+		for (int j = 0; j < tensor->size() / 2; j++) {
+			for (int i = 0; i < tensor->size() - 1; i++) {
+				tensor->move_out();
+			}
+			tensor->move_down();
+			for (int i = tensor->size() - 1; i > 0; i--) {
+				tensor->move_in();
+			}
+			tensor->move_down();
+		}
+		tensor->move_right();
+		for (int j = 0; j < tensor->size() / 2; j++) {
+			for (int i = 0; i < tensor->size() - 1; i++) {
+				tensor->move_out();
+			}
+			tensor->move_up();
+			for (int i = tensor->size() - 1; i > 0; i--) {
+				tensor->move_in();
+			}
+			tensor->move_up();
+		}
+		tensor->move_right();
+	}
+}
+void tensor_kji_order_z(tensor_walker *tensor) {
+	tensor->teleport(0, 0, 0);
+	for (int k = 0; k < tensor->size() / 2; k++) {
+		for (int j = 0; j < tensor->size() / 2; j++) {
+			for (int i = 0; i < tensor->size() - 1; i++) {
+				tensor->move_out();
+			}
+			tensor->move_down();
+			for (int i = tensor->size() - 1; i > 0; i--) {
+				tensor->move_in();
+			}
+			tensor->move_down();
+		}
+		tensor->move_right();
+		for (int j = 0; j < tensor->size() / 2; j++) {
+			for (int i = 0; i < tensor->size() - 1; i++) {
+				tensor->move_out();
+			}
+			tensor->move_up();
+			for (int i = tensor->size() - 1; i > 0; i--) {
+				tensor->move_in();
+			}
+			tensor->move_up();
+		}
+		tensor->move_right();
+	}
+}
+void tensor_kji_order_hilbert(tensor_walker *tensor) {
 	tensor->teleport(0, 0, 0);
 	for (int k = 0; k < tensor->size() / 2; k++) {
 		for (int j = 0; j < tensor->size() / 2; j++) {
@@ -105,15 +265,15 @@ int main() {
 	ht->set_default();
 
 	fprintf(stderr, "Benchmarking naive_tensor_walker...\n");
-	tensor_rand_move(nt);
-	tensor_ijk_order(nt);
-	tensor_kji_order(nt);
+	tensor_rand_move_naive(nt);
+	tensor_ijk_order_z(nt);
+	tensor_kji_order_hilbert(nt);
 	fprintf(stderr, "Benchmarking z_tensor_walker...\n");
-	tensor_rand_move(zt);
-	tensor_ijk_order(zt);
-	tensor_kji_order(zt);
+	tensor_rand_move_naive(zt);
+	tensor_ijk_order_z(zt);
+	tensor_kji_order_hilbert(zt);
 	fprintf(stderr, "Benchmarking hilbert_tensor_walker...\n");
-	tensor_rand_move(ht);
-	tensor_ijk_order(ht);
-	tensor_kji_order(ht);
+	tensor_rand_move_naive(ht);
+	tensor_ijk_order_z(ht);
+	tensor_kji_order_hilbert(ht);
 }
